eadafd22f06266a791295b21aa3a7f7db023ec9e
Mason Desai <masonpdesai@gmail.com>
2023-12-17 23:49:19 -0600

    I FINALLY DID IT EVERYONE AFTER WATCHING ENOUGH YOUTUBE VIDEOS AND READING W3 SCHOOLS AND USING EVERY ASSET AT MY DISPOSAL I FIGURED OUT HOW TO GET THE API WORKING IN THE SEARCH BAR PLEASE GIVE ME SOME APPLAUSE

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,35 @@
-const userCardTemplate = document.querySelector("[data-user-template]")
 
-fetch("https://en.wikipedia.org/w/api.php?action=query&origin=*&format=json&generator=search&gsrnamespace=0&gsrlimit=5&gsrsearch='Reptiles'")
-.then(res => res.json())
-.then(data => {
-    data.forEach(user => {
-        const card = userCardTemplate.textContent.cloneNode(true).children[0]
-        const header = card.querySelector("[data-header]")
-        const body = card.querySelector("[data-body]")
-        console.log(user )
+function searchWikipedia() {
+    const searchTerm = document.getElementById('searchInput').value;
+    const apiUrl = `https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=${searchTerm}&callback=?`;
+
+    $.ajax({
+      url: apiUrl,
+      dataType: 'jsonp',
+      success: function (data) {
+        displaySearchResults(data.query.search);
+      },
+    error: function (error) {
+        console.error('Error fetching data from Wikipedia API:', error);
+      }
     });
-})
-    //"https://en.wikipedia.org/w/api.php?action=query&origin=*&format=json&generator=search&gsrnamespace=0&gsrlimit=5&gsrsearch='Reptiles'"
+  }
+
+  function displaySearchResults(results) {
+    const searchResultsContainer = document.getElementById('searchResults');
+    searchResultsContainer.innerHTML = '';
+
+
+    results.forEach(result => {
+      const resultElement = document.createElement('div');
+      resultElement.innerHTML = `
+      <h3>${result.title}</h3>
+      <p>${result.snippet}</p>
+      <hr> `;
+
+      searchResultsContainer.appendChild(resultElement);
+    });
+  }
 
 
 
